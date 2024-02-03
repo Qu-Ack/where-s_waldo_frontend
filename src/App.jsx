56b0603515b9a17ae0b3942acba7a12740697223
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2024-02-04 01:46:41 +0530

    ADDED dropdown on click

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,30 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [isVisible , setIsVisible] = useState(false);
+  const [position , setPosition] = useState({x:0, y:0});
+
+  function handleMouseDown(e) {
+    setIsVisible(true)
+    let x_coordinate = e.clientX;
+    let y_coordinate = e.clientY;
+    setPosition({x:x_coordinate, y:y_coordinate});
+    console.log(position)    
+  }
+
 
 
   return (
-    <div className="image">
-      
+    <div className="image" onMouseDown={handleMouseDown} style={{position:'relative'}} >
+      {isVisible && (
+        <div style={{position:'absolute', top:position.y, left:position.x, color:'red', background:'black'}}> 
+          <ul className='dropdown'>
+            <li className='option'>Bob</li>
+            <li className='option'>Rob</li>
+            <li className='option'>Lob</li>
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
